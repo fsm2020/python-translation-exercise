3cd07d71be35c40a0995e1fe5a87d30a5bf153d6
Frank S. Moen <fsm0005@auburn.edu>
2020-03-18 13:18:54 -0500

    Edited and Corrected translate_sequence and get_all_translations Modified reverse_and_complement

--- translate.py
+++ translate.py
@@ -4,32 +4,45 @@ import sys
 
 def translate_sequence(rna_sequence, genetic_code):
     """Translates a sequence of RNA into a sequence of amino acids.
-
     Translates `rna_sequence` into string of amino acids, according to the
     `genetic_code` given as a dict. Translation begins at the first position of
     the `rna_sequence` and continues until the first stop codon is encountered
     or the end of `rna_sequence` is reached.
-
     If `rna_sequence` is less than 3 bases long, or starts with a stop codon,
     an empty string is returned.
     """
+    rna_seqs = rna_sequence.upper()
+    start_pos = 0
+    proteins = ''
+    for i in range(start_pos, len(rna_seqs), 3):
+        codon = rna_seqs[i:i + 3]
+        if codon in ['UAG', 'UAA', 'UGA'] or len(codon) != 3:
+            break
+        else: proteins += genetic_code[codon]
+    return proteins
     pass
 
 def get_all_translations(rna_sequence, genetic_code):
-    """Get a list of all amino acid sequences encoded by an RNA sequence.
+    rna_seqs = rna_sequence.upper()
+    aa_list = []
+    start_pos = 0
 
-    All three reading frames of `rna_sequence` are scanned from 'left' to
-    'right', and the generation of a sequence of amino acids is started
-    whenever the start codon 'AUG' is found. The `rna_sequence` is assumed to
-    be in the correct orientation (i.e., no reverse and/or complement of the
-    sequence is explored).
+    def translate(start_pos,rna_seqs,genetic_code):
+        proteins = ''
+        for i in range(start_pos, len(rna_seqs), 3):
+            codon = rna_seqs[i:i + 3]
+            if codon in ['UAG', 'UAA', 'UGA'] or len(codon) != 3:
+                break
+            else: proteins += genetic_code[codon]
+        return proteins
 
-    The function returns a list of all possible amino acid sequences that
-    are encoded by `rna_sequence`.
-
-    If no amino acids can be translated from `rna_sequence`, an empty list is
-    returned.
-    """
+    while start_pos < len(rna_seqs):
+        start_codon = rna_seqs[start_pos:start_pos + 3]
+        if start_codon == 'AUG':
+            translation = translate(start_pos, rna_seqs, genetic_code)
+            aa_list.append(translation)
+        start_pos += 1
+    return aa_list
     pass
 
 def get_reverse(sequence):
@@ -55,7 +68,8 @@ def reverse_and_complement(sequence):
     of `sequence`.
     If `sequence` is empty, an empty string is returned.
     """
-    return get_reverse(get_complement(sequence))
+    rev_comp = get_reverse(get_complement(sequence))
+    return rev_comp
     pass
 
 def get_longest_peptide(rna_sequence, genetic_code):
@@ -69,8 +83,32 @@ def get_longest_peptide(rna_sequence, genetic_code):
     If no amino acids can be translated from `rna_sequence` nor its reverse and
     complement, an empty string is returned.
     """
-    pass
+    rna_seqs = rna_sequence.upper()
+    rev_comp = get_reverse(get_complement(rna_seqs)
+    aa_list = []
+    start_pos = 0
 
+    def rev_compRNA (rna_seqs)
+        rev_compRNAs = get_reverse(get_complement(rna_seqs))
+        return rev_compRNAs
+
+    def translate(rev_compRNAS,rna_seqs,genetic_code):
+        proteins = ''
+        for i in range(start_pos, len(rna_seqs), 6):
+            codon = rna_seqs[i:i + 6]
+            if codon in ['UAG', 'UAA', 'UGA'] or len(codon) != 6:
+                break
+            else: proteins += genetic_code[codon]
+        return proteins
+
+    while start_pos < len(rna_seqs):
+        start_codon = rna_seqs[start_pos:start_pos + 6]
+        if start_codon == 'AUG':
+            translation = translate(start_pos, rna_seqs, genetic_code)
+            aa_list.append(translation)
+        start_pos += 1
+    return aa_list
+    pass
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
